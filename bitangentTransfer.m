509fb011d989c7cb06cdc5302c757283ddfa2588
oscar-aucatoma <oscarap06@hotmail.com>
2026-04-03 00:04:37 +0200

    Bitangent Transfer

--- bitangentTransfer.m
+++ bitangentTransfer.m
@@ -3,6 +3,7 @@ function [DeltaV1, DeltaV2, Deltat] = bitangentTransfer(a_i, e_i,a_f, e_f,type,m
 %
 % [DeltaV1, DeltaV2, Deltat] = bitangentTransfer(a_i, e_i,a_f, e_f,type,mu)
 %
+% -----------------------------------------------------------------------------------------------
 % Input Arguments:
 % ai    [1x1] initial semi-major axis           [km]
 % ei    [1x1] initial eccentricity              [-]
@@ -11,25 +12,30 @@ function [DeltaV1, DeltaV2, Deltat] = bitangentTransfer(a_i, e_i,a_f, e_f,type,m
 % type  [char] maneuver type                   
 % mu    [1x1] gravitational parameter           [km^3/s^2]
 %
+% -----------------------------------------------------------------------------------------------
 % Output Arguments: 
 % DeltaV1    [1x1] first maneuver impulse       [km/s]
 % DeltaV2    [1x1] second maneuver impulse      [km/s]
 % Deltat     [1x1] maneuver time                [s]
+%
 
-if type == "pa" % from Pericenter to Apocenter
+% from Pericenter to Apocenter
+if type == "pa" 
     rp_T = a_i * (1-e_i); % Pericenter Radius Transfer Orbit = Pericenter Radius Initial Orbit
     ra_T = a_f*(1+e_f); % Apocenter Radius Transfer Orbit = Apocenter Radius Final Orbit
     a_T = (ra_T+rp_T) / 2; % Semi-major Axis Transfer Orbit
     DeltaV1 = sqrt(mu) * sqrt(2/rp_T-1/a_T) - sqrt(mu) *sqrt(2/rp_T-1/a_i);
     DeltaV2 = sqrt(mu) * sqrt(2/ra_T-1/a_f) - sqrt(mu) *sqrt(2/ra_T-1/a_T);
 
-elseif  type == "ap" % from Apocenter to Pericenter
+% from Apocenter to Pericenter
+elseif  type == "ap" 
     ra_T = a_i * (1+e_i); % Apocenter Radius Transfer Orbit = Apocenter Radius Initial Orbit
     rp_T = a_f * (1-e_f); % Pericenter Radius Transfer Orbit = Pericenter Radius Final Orbit
     a_T = (ra_T+rp_T) / 2; % Semi-major Axis Transfer Orbit
     DeltaV1 = sqrt(mu) * sqrt(2/ra_T - 1/a_T)-sqrt(mu) *sqrt(2/ra_T-1/a_i);
     DeltaV2 = sqrt(mu) * sqrt(2/rp_T-1/a_f) - sqrt(mu) * sqrt(2/rp_T-1/a_T);
 
+% from Pericenter to Pericenter
 elseif type == "pp"
     rp_T = a_i * (1-e_i); % Transfer Pericenter Radius =  Initial Pericenter Radius
     ra_T = a_f * (1-e_f); % Transfer Apocenter Radius = Final Pericenter Radius
@@ -37,6 +43,7 @@ elseif type == "pp"
     DeltaV1 = sqrt(mu) * sqrt(2/rp_T - 1/a_T)-sqrt(mu) *sqrt(2/rp_T-1/a_i);
     DeltaV2 = sqrt(mu) * sqrt(2/ra_T-1/a_f) - sqrt(mu) * sqrt(2/ra_T-1/a_T);
 
+% from Apocenter to Apocenter
 elseif  type =="aa"
     rp_T = a_i * (1+e_i); % Transfer Pericenter Radius = Initial Apocenter Radius 
     ra_T = a_f * (1+e_f); % Transfer Apocenter Radius = Final Apocenter Radius
@@ -45,8 +52,8 @@ elseif  type =="aa"
     DeltaV2 = sqrt(mu) * sqrt(2/ra_T-1/a_f) - sqrt(mu) * sqrt(2/ra_T-1/a_T);
 end
 
-Deltat = pi* sqrt( (a_T)^3 / mu ); % Time for the Maneuver
-
+% Time for the Maneuver
+Deltat = pi* sqrt( (a_T)^3 / mu );
 
 end
 
