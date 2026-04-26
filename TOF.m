6b69580c930fdeb3e2806f62e0884663dcd0e13f
oscar-aucatoma <oscarap06@hotmail.com>
2026-04-26 15:05:50 +0200

    TOF function

--- TOF.m
+++ TOF.m
@@ -16,20 +16,22 @@ function [deltat] = TOF(a, e, th1, th2, mu)
 % deltat [1x1]  time of flight               [s]
 % -----------------------------------------------
 
+th1 = mod(th1,2*pi);
+th2 = mod(th2,2*pi); 
+% il comando mod mi restituisce il resto della divisione ex: mod(7*pi, 2*pi)  = pi, in modo che sia compreso tra 0 e 2*pi
 
-E1 = 2 * atan(sqrt((1-e)/(1+e))*tan(th1/2));
-E2 = 2 * atan(sqrt((1-e)/(1+e))*tan(th2/2));
+
+% Problema: atan restituisce solo angoli compresi tra -pi/2 e pi/2, il resto non viene considerato --> utilizzare atan2
+E1 = 2 * atan2(sqrt(1-e)*sin(th1/2), sqrt(1+e)*cos(th1/2));
+E2 = 2 * atan2(sqrt(1-e)*sin(th2/2), sqrt(1+e)*cos(th2/2));
+
+% atan2 restituisce un angolo
+% tra 0 e pi--> moltiplicando per 2
+% otterrò sempre un angolo tra [0 2*pi] :)
 
 % If th1 or th2 are greater than pi E needs to be corrected in order for
 % the atan to give the correct anomaly
 
-if th1 > pi
-    E1 = E1 + 2 * pi; % Adjust E1 for the case when th1 > pi
-end
-
-if th2 > pi
-    E2 = E2 + 2 * pi; % Adjust E2 for the case when th2 > pi
-end
 
 % Case when th2<th1: a whole period needs to be added
 
