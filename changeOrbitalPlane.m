4607bf1ad3384551f9ef5b0944d1019e7af7bef3
oscar-aucatoma <oscarap06@hotmail.com>
2026-04-26 15:07:06 +0200

    Change Orbital Plane function

--- changeOrbitalPlane.m
+++ changeOrbitalPlane.m
@@ -17,7 +17,7 @@ function [DeltaV, omf, theta] = changeOrbitalPlane(a, e, i_i, OMi, omi, i_f, OMf
 % ----------------------------------------------------------------------------
 % Output arguments:
 % DeltaV                [1x1]   maneuver impulse
-% omi                   [1x1]   final pericenter anomaly
+% omf                   [1x1]   final pericenter anomaly
 % theta                 [1x1]   true anomaly at maneuver
 
 % solution of the sferical triangle
@@ -26,26 +26,64 @@ Deltai =  i_f - i_i;            % initial inclinatio variation
 
 % ui, uf triangle sides and alfa corner between the two sides
 alfa = acos(cos(i_i)*cos(i_f) + sin(i_i)*sin(i_f)*cos(abs(DeltaOM)));
-ui = atan2(sin(abs(DeltaOM))/sin(alfa)*sin(i_f), (-cos(i_f) + cos(alfa)*cos(i_i))/sin(alfa)/sin(i_i));
-uf = atan2(sin(abs(DeltaOM))/sin(alfa)*sin(i_i), (cos(i_i) - cos(alfa)*cos(i_f))/sin(alfa)/sin(i_f));
+sin_ui = sin(abs(DeltaOM))/sin(alfa)*sin(i_f);
+sin_uf = sin(abs(DeltaOM))/sin(alfa)*sin(i_i);
 
 if (DeltaOM > 0 && Deltai > 0)
+        
+    cos_ui = (-cos(i_f)+cos(alfa)*cos(i_i)) / (sin(alfa)*sin(i_i));
+    cos_uf = (cos(i_i)-cos(alfa)*cos(i_f)) / (sin(alfa)*sin(i_f));
+    
+    ui = atan2(sin_ui, cos_ui);
+    uf = atan2(sin_uf, cos_uf);
+
     theta = ui - omi;
     omf = uf - theta;
+
 elseif (DeltaOM > 0 && Deltai < 0)
-        theta = 2*pi - ui - omi;
-        omf = 2*pi - uf - theta;
-elseif (DeltaOM < 0 && Deltai < 0)
-        theta = 2*pi - ui - omi;
-        omf = 2*pi - uf - theta;
+
+    cos_ui = (cos(i_f)-cos(alfa)*cos(i_i)) / (sin(alfa)*sin(i_i));
+    cos_uf = (-cos(i_i)+cos(alfa)*cos(i_f)) / (sin(alfa)*sin(i_f));
+    
+    ui = atan2(sin_ui, cos_ui);
+    uf = atan2(sin_uf, cos_uf);
+
+    theta = 2*pi - ui - omi;
+    omf = 2*pi - uf - theta;
+
+elseif (DeltaOM < 0 && Deltai > 0)
+
+
+    cos_ui = (-cos(i_f)+cos(alfa)*cos(i_i)) / (sin(alfa)*sin(i_i));
+    cos_uf = (cos(i_i)-cos(alfa)*cos(i_f)) / (sin(alfa)*sin(i_f));
+   
+
+    ui = atan2(sin_ui, cos_ui);
+    uf = atan2(sin_uf, cos_uf);
+
+    theta = 2*pi - ui - omi;
+    omf = 2*pi - uf - theta;
 else
+
+    cos_ui = (cos(i_f)-cos(alfa)*cos(i_i)) / (sin(alfa)*sin(i_i));
+    cos_uf = (-cos(i_i)+cos(alfa)*cos(i_f)) / (sin(alfa)*sin(i_f));
+
+    ui = atan2(sin_ui, cos_ui);
+    uf = atan2(sin_uf, cos_uf);
+
     theta = ui - omi;
     omf = uf - theta;
 end
+
+
+% NOI VOGLIAMO SEMPRE L'ANGOLO COMPRESO TRA 0 E 2*PI !!!
+theta = mod(theta,2*pi);
+omf = mod(omf,2*pi);
+
 p = a*(1 - e^2);
 
 if (cos(theta) > 0)
-    theta = theta + pi;
+    theta = mod(theta + pi,2*pi);
     disp('conviene manovrare nel punto di intersezione tra le orbite theta + pi');
     Vtheta2 = sqrt(mu/p)*(1 + e*cos(theta));
     DeltaV = 2*Vtheta2*sin(alfa/2);
@@ -56,4 +94,3 @@ end
 
 
 end
-
